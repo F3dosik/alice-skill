725541b3192779cffcbfbe0ba126691cccd28b74
F3dosik <izhevsk2073@gmail.com>
2025-09-24 21:24:00 +0400

    Добавлена возможность задания адреса по флагу

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
+	parseFlags()
+
 	if err := run(); err != nil {
 		panic(err)
 	}
 }
 
 func run() error {
-	return http.ListenAndServe(`:8080`, http.HandlerFunc(webhook))
+	fmt.Println("Running server on", flagRunAddr)
+	return http.ListenAndServe(flagRunAddr, http.HandlerFunc(webhook))
 }
 
 func webhook(w http.ResponseWriter, r *http.Request) {
@@ -23,11 +27,11 @@ func webhook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	_, _ = w.Write([]byte(`
-		{
-			"response": {
-				"text": "Извините, я пока ничего не умею"
-			},
-			"version": "1.0"
-		}
+{
+	"response": {
+		"text": "Извините, я пока ничего не умею"
+	},
+	"version": "1.0"
+}
 	`))
 }
